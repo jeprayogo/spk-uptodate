3e52634a9c578b83df34c9e7eb7f326b9b0a7f20
Janny Eka Prayoga <jannyekaprayogo@gmail.com>
2026-01-17 10:30:06 +0700

    update

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -6,10 +6,6 @@ import { Pool } from 'pg'
 // PostgreSQL pool (runtime connection)
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? { rejectUnauthorized: false }
-      : false,
 })
 
 // Prisma driver adapter
